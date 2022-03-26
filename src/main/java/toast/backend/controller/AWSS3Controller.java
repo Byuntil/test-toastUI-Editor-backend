f38ba271343376bb130a17f997e9b98a72d8ca9e
Byuntil <gugugu2014@jbnu.ac.kr>
2022-03-26 09:39:26 +0900

    first commit

---
+++ src/main/java/toast/backend/controller/AWSS3Controller.java
@@ -0,0 +1,24 @@
+package toast.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import toast.backend.service.FileUploadService;
+
+@RestController
+@RequestMapping("/s3")
+@RequiredArgsConstructor
+public class AWSS3Controller {
+    private final FileUploadService fileUploadService;
+
+    //이미지 조회
+
+    //이미지 업로드
+    @PostMapping("/upload")
+    public String uploadImage(@RequestPart MultipartFile file){
+        return fileUploadService.uploadImage(file);
+    }
+
+    //이미지 삭제
+
+}
---
+++ src/main/java/toast/backend/service/AWSS3UploadService.java
@@ -0,0 +1,27 @@
+package toast.backend.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@RequiredArgsConstructor
+@Component
+public class AWSS3UploadService implements UploadService{
+
+    private final AmazonS3 amazonS3;
+    private final S3Component componet;
+    @Override
+    public void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(componet.getBucket(), fileName, inputStream, objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    @Override
+    public String getFileUrl(String fileName) {
+        return amazonS3.getUrl(componet.getBucket(), fileName).toString();
+    }
+}
